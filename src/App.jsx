6f953e669c50939b0f38305e1d2c1e5b0fe42fa3
EyadOmar <eiad.omar88@gmail.com>
2023-12-12 20:14:39 +0200

    added globals

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>app</div>;
+  return <div className="container">app</div>;
 }
 
 export default App;
