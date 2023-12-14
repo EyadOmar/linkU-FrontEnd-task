5be1249504c98f0ed8599011d5f77e42c97ddc6c
EyadOmar <eiad.omar88@gmail.com>
2023-12-14 22:34:31 +0200

    fixed hero slider on mobile

--- src/pages/home/hero/HeroSlider.jsx
+++ src/pages/home/hero/HeroSlider.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable react/prop-types */
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useMediaQuery } from '@react-hook/media-query';
 import './slider.css';
+import { useEffect } from 'react';
 
 function HeroSlider({ imgsSrc }) {
+  const isMobile = useMediaQuery('(max-width: 768px)');
   const sliderSettings = {
     className: 'slider',
-    slidesPerView: 1.1,
+    slidesPerView: isMobile ? 1 : 1.1,
     scrollbar: true,
     loop: true,
     centeredSlides: true,
+    spaceBetween: isMobile ? 0 : 10,
   };
+
+  useEffect(() => {}, [isMobile]);
 
   return (
     <Swiper {...sliderSettings} dir="ltr">
